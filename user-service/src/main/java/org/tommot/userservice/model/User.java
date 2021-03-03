276c385bc2951dca0009e3ca7f0d2307d70fd91e
Tosin <tosintubi@users.noreply.github.com>
2021-03-04 00:12:12 +0100

    User model

---
+++ user-service/src/main/java/org/tommot/userservice/model/User.java
@@ -0,0 +1,15 @@
+package org.tommot.userservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
