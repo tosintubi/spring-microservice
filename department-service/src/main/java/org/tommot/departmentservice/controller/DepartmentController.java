a08306e84cd0c1769d9b08bc18db8eb2259d66ef
Tosin <tosintubi@users.noreply.github.com>
2021-03-03 18:17:44 +0100

    Department Controller Test

--- department-service/src/main/java/org/tommot/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/org/tommot/departmentservice/controller/DepartmentController.java
@@ -1,11 +1,10 @@
 package org.tommot.departmentservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tommot.departmentservice.model.Department;
 import org.tommot.departmentservice.service.DepartmentService;
 
@@ -13,11 +12,19 @@ import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/api/v1/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+
+    @PostMapping("/add")
     public ResponseEntity<Department> saveDepartment(Department department){
         Department newDepartment = departmentService.saveDepartment(department);
         return new ResponseEntity<>(newDepartment, HttpStatus.CREATED);
