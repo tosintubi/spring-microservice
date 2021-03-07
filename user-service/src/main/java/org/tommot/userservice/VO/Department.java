45234cfa3d211836d9f6493241ba24233357987a
Tosin <tosintubi@users.noreply.github.com>
2021-03-08 00:08:22 +0100

    Department VO POJO

---
+++ user-service/src/main/java/org/tommot/userservice/VO/Department.java
@@ -0,0 +1,13 @@
+package org.tommot.userservice.VO;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Department {
+
+        private Long departmentId;
+        private String departmentName;
+        private String departmentAddress;
+        private String departmentCode;
+}
