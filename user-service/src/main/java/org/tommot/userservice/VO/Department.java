0a9d4ca6fc45e38f90f4a618ae6a4df1b6c1ce30
Tosin <tosintubi@users.noreply.github.com>
2021-03-08 00:10:59 +0100

    User-DepartmentResponseTemplateVO

--- user-service/src/main/java/org/tommot/userservice/VO/Department.java
+++ user-service/src/main/java/org/tommot/userservice/VO/Department.java
@@ -1,9 +1,12 @@
 package org.tommot.userservice.VO;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
 
         private Long departmentId;
---
+++ user-service/src/main/java/org/tommot/userservice/VO/ResponseTemplateVO.java
@@ -0,0 +1,14 @@
+package org.tommot.userservice.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.tommot.userservice.model.User;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+}
