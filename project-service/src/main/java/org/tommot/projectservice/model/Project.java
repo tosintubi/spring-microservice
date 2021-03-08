89d25f64c3217b2f9e1ab7e9d2f2738500405b60
Tosin <tosintubi@users.noreply.github.com>
2021-03-08 02:32:56 +0100

    generatedValue for @Id

--- project-service/src/main/java/org/tommot/projectservice/model/Project.java
+++ project-service/src/main/java/org/tommot/projectservice/model/Project.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -14,6 +16,7 @@ import javax.persistence.Id;
 public class Project {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String projectCode;
     private String projectTitle;
