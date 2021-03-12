855ecf743ec8c745fee9e7bfccb9ab23cc67a819
Tosin <tosintubi@users.noreply.github.com>
2021-03-12 23:58:30 +0100

    Department entity optimize

--- department-service/src/main/java/org/tommot/departmentservice/model/Department.java
+++ department-service/src/main/java/org/tommot/departmentservice/model/Department.java
@@ -3,7 +3,6 @@ package org.tommot.departmentservice.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
