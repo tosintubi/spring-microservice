28d896d150857065ebb2409ad250e7313bbd601a
tosintubi <tosintubi@users.noreply.github.com>
2021-03-14 17:41:37 +0100

    cloud gateway initial commit

---
+++ cloud-gateway/src/test/java/org/tommot/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommot.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
