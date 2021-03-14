84a17c39ba5cef439d9db378b7b297bc9088bdd8
tosintubi <tosintubi@users.noreply.github.com>
2021-03-14 23:50:03 +0100

    Enabling Hystrix Dashboard

--- hystrix-dashboard/src/main/java/org/tommot/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/org/tommot/hystrixdashboard/HystrixDashboardApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrix
+@EnableHystrixDashboard
 @EnableEurekaClient
 public class HystrixDashboardApplication {
 
