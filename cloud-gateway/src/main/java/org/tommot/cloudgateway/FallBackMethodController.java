51b9c26651e6154b8bcccf4d67478c83d722d641
tosintubi <tosintubi@users.noreply.github.com>
2021-03-14 22:43:43 +0100

    Controller's FallBackMethods

---
+++ cloud-gateway/src/main/java/org/tommot/cloudgateway/FallBackMethodController.java
@@ -0,0 +1,24 @@
+package org.tommot.cloudgateway;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBack(){
+        return "User service is taking longer than expected. Please try again later";
+    }
+
+    @GetMapping("/projectServiceFallBack")
+    public String projectServiceFallBack(){
+        return "Project service is taking longer than expected. Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBack(){
+        return "Department service is taking longer than expected. Please try again later";
+    }
+}
